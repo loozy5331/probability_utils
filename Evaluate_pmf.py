1b42b27f27033ef2a49c4f789eb33f929758cc9a
loozy <time8970@naver.com>
2018-11-07 00:01:09 +0900

    Update Evaluate_pmf.py
    R로만 얻은 precision, recall
    BGR로 얻은 precision, recall 저장
    병렬 연산

--- Evaluate_pmf.py
+++ Evaluate_pmf.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import json
+from multiprocessing import Pool
 
 os.chdir(r"C:\Users\time8\Desktop\program\2018.10")
 cur_path = os.getcwd()
@@ -29,26 +30,26 @@ def make_pmf(freq):
     return freq
 
 
-def Test_SkinOrNonSkin_R(file_set, skin_likelihood, NonSkin_likelihood, prior):
+def Test_SkinOrNonSkin_R(file_set, skin_likelihood, NonSkin_likelihood, count):
+    PRIOR = 0.5
     TP = 0
     TN = 0
     FP = 0
     FN = 0
     skin_R = skin_likelihood.iloc[2, :]
     NonSkin_R = NonSkin_likelihood.iloc[2, :]
-
+    print(count)
     for path in file_set:
         try:
             file = pd.read_csv(path, header=None)
         except(Exception):
             continue
-        print(TP, TN, FP, FN)
         row = len(file)  # 행 길이
         for i in range(row):
             real_val = file.iloc[i, -1]
             R = file.iloc[i, 2]
-            skin_posterior = skin_R[R] * prior
-            Nonskin_posterior = NonSkin_R[R] * (1 - prior)
+            skin_posterior = skin_R[R] * PRIOR
+            Nonskin_posterior = NonSkin_R[R] * (1 - PRIOR)
 
             # skin 이고, 실제로 skin
             if((skin_posterior > Nonskin_posterior) and real_val == 1):
@@ -63,28 +64,56 @@ def Test_SkinOrNonSkin_R(file_set, skin_likelihood, NonSkin_likelihood, prior):
             else:
                 FN += 1
 
-    return TP, TN, FP, TN;
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    temp = str(precision) + ", " + str(recall)
+    with open(r"Evaluate_data\precision_recall_set_{}_R.csv".format(count + 1)) as f:
+        f.write(temp)
 
-def Test_SkinOrNonSkin(file_set, skin_post, NonSkin_post):
+def Test_SkinOrNonSkin(file_set, skin_likelihood, NonSkin_likelihood, count):
+    PRIOR = 0.5
     TP = 0
     TN = 0
     FP = 0
     FN = 0
-    
-    for count, path in enumerate(file_set):
+    skin_B = skin_likelihood.iloc[0, :]
+    skin_G = skin_likelihood.iloc[1, :]
+    skin_R = skin_likelihood.iloc[2, :]
+
+    NonSkin_B = NonSkin_likelihood.iloc[0, :]
+    NonSkin_G = NonSkin_likelihood.iloc[1, :]
+    NonSkin_R = NonSkin_likelihood.iloc[2, :]
+    print(count)
+    for path in file_set:
         try:
             file = pd.read_csv(path, header=None)
         except(Exception):
             continue
-        count+=1
-        print(count)
         row = len(file)  # 행 길이
         for i in range(row):
             real_val = file.iloc[i, -1]
             B = file.iloc[i, 0]
             G = file.iloc[i, 1]
             R = file.iloc[i, 2]
-
+            skin_posterior = (skin_B[B] * skin_G[G] * skin_R[R]) * PRIOR
+            Nonskin_posterior = (NonSkin_B[B] * NonSkin_G[G] * NonSkin_R[R]) * (1 - PRIOR)
+            # skin 이고, 실제로 skin
+            if ((skin_posterior > Nonskin_posterior) and real_val == 1):
+                TP += 1
+            # Nonskin 이고, 실제로 Nonskin
+            elif ((skin_posterior < Nonskin_posterior) and real_val != 1):
+                TN += 1
+            # skin 이고, 실제론 Nonskin
+            elif ((skin_posterior > Nonskin_posterior) and real_val != 1):
+                FP += 1
+            # Nonskin 이고, 실제로 skin
+            else:
+                FN += 1
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    temp = str(precision) + ", " + str(recall)
+    with open(r"Evaluate_data\precision_recall_set_{}.csv".format(count + 1)) as f:
+        f.write(temp)
 
 
 if __name__ == "__main__":
@@ -94,18 +123,26 @@ if __name__ == "__main__":
     Nonskin_freq = [pd.read_csv(r"pmf_set\NonS_BGR_{}_freq.csv".format(count + 1), header=None) for count in range(5)]
 
     # Set별 pmf들을 만듬.
-    skin_pmf_1 = make_pmf(skin_freq[1] + skin_freq[2] + skin_freq[3] + skin_freq[4])
-    skin_pmf_2 = make_pmf(skin_freq[0] + skin_freq[2] + skin_freq[3] + skin_freq[4])
-    skin_pmf_3 = make_pmf(skin_freq[0] + skin_freq[1] + skin_freq[3] + skin_freq[4])
-    skin_pmf_4 = make_pmf(skin_freq[0] + skin_freq[1] + skin_freq[2] + skin_freq[4])
-    skin_pmf_5 = make_pmf(skin_freq[0] + skin_freq[1] + skin_freq[2] + skin_freq[3])
+    skin_pmf = [make_pmf(skin_freq[1] + skin_freq[2] + skin_freq[3] + skin_freq[4])
+                , make_pmf(skin_freq[0] + skin_freq[2] + skin_freq[3] + skin_freq[4])
+                , make_pmf(skin_freq[0] + skin_freq[1] + skin_freq[3] + skin_freq[4])
+                , make_pmf(skin_freq[0] + skin_freq[1] + skin_freq[2] + skin_freq[4])
+                , make_pmf(skin_freq[0] + skin_freq[1] + skin_freq[2] + skin_freq[3])]
 
-    Non_skin_pmf_1 = make_pmf(Nonskin_freq[1] + Nonskin_freq[2] + Nonskin_freq[3] + Nonskin_freq[4])
-    Non_skin_pmf_2 = make_pmf(Nonskin_freq[0] + Nonskin_freq[2] + Nonskin_freq[3] + Nonskin_freq[4])
-    Non_skin_pmf_3 = make_pmf(Nonskin_freq[0] + Nonskin_freq[1] + Nonskin_freq[3] + Nonskin_freq[4])
-    Non_skin_pmf_4 = make_pmf(Nonskin_freq[0] + Nonskin_freq[1] + Nonskin_freq[2] + Nonskin_freq[4])
-    Non_skin_pmf_5 = make_pmf(Nonskin_freq[0] + Nonskin_freq[1] + Nonskin_freq[2] + Nonskin_freq[3])
+    Non_skin_pmf = [make_pmf(Nonskin_freq[1] + Nonskin_freq[2] + Nonskin_freq[3] + Nonskin_freq[4])
+                    , make_pmf(Nonskin_freq[0] + Nonskin_freq[2] + Nonskin_freq[3] + Nonskin_freq[4])
+                    , make_pmf(Nonskin_freq[0] + Nonskin_freq[1] + Nonskin_freq[3] + Nonskin_freq[4])
+                    , make_pmf(Nonskin_freq[0] + Nonskin_freq[1] + Nonskin_freq[2] + Nonskin_freq[4])
+                    , make_pmf(Nonskin_freq[0] + Nonskin_freq[1] + Nonskin_freq[2] + Nonskin_freq[3])]
 
-    a, b, c, d = Test_SkinOrNonSkin_R(file_list[0], skin_pmf_1, Non_skin_pmf_1, 0.5)
-    print(a, b, c, d)
+    count_list = list(range(5))
+    # R로만 precision, recall을 구함.
+    #with Pool(12) as p:
+    #    p.starmap(Test_SkinOrNonSkin_R, zip(file_list, skin_pmf, Non_skin_pmf, count_list))
+
+    # BGR로 precision, recall을 구함.
+    with Pool(12) as p:
+        p.starmap(Test_SkinOrNonSkin, zip(file_list, skin_pmf, Non_skin_pmf, count_list))
+
+
 
